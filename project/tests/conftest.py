61cc1068958ef7c6ff4761451e0c7c2d09df13d3
jpvramos <joaopaulovieiraramos@gmail.com>
2020-11-18 18:34:29 -0300

    fix: Web container

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -4,16 +4,15 @@
 import os
 
 import pytest
-from app.config import Settings, get_settings
-from app.main import create_application
 from starlette.testclient import TestClient
 from tortoise.contrib.fastapi import register_tortoise
 
+from app.config import Settings, get_settings
+from app.main import create_application
+
 
 def get_settings_override():
-    return Settings(
-        testing=1, database_url=os.environ.get("DATABASE_TEST_URL")
-    )
+    return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
 
 
 @pytest.fixture(scope="module")
