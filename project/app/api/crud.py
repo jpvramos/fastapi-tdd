023217ee1e1f300457f993efdff52697267b03d9
jpvramos <joaopaulovieiraramos@gmail.com>
2020-11-19 21:46:12 -0300

    fix: test

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -33,7 +33,6 @@ async def put(id: int, payload: SummaryPayLoadSchema) -> Union[dict, None]:
     summary = await TextSummary.filter(id=id).update(
         url=payload.url, summary=payload.summary
     )
-    
     if summary:
         updated_summary = await TextSummary.filter(id=id).first().values()
         return updated_summary[0]
